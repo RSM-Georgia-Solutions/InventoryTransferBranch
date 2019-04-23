6d166cd9c6b9745c9294c634a62e90c70d84e6c7
nikaku <qurdadzenika@gmail.com>
2019-04-23 19:57:38 +0400

    Add project files.

---
+++ InventoryTransferBranch/Delivery.b1f.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+using SAPbobsCOM;
+using SAPbouiCOM;
+using SAPbouiCOM.Framework;
+using Application = SAPbouiCOM.Framework.Application;
+
+namespace InventoryTransferBranch
+{
+    [FormAttribute("140", "Delivery.b1f")]
+    class Delivery : SystemFormBase
+    {
+        public Delivery()
+        {
+        }
+
+        /// <summary>
+        /// Initialize components. Called by framework after form created.
+        /// </summary>
+        public override void OnInitializeComponent()
+        {
+            this.Button0 = ((SAPbouiCOM.Button)(this.GetItem("Item_3333").Specific));
+            this.Button0.PressedAfter += new SAPbouiCOM._IButtonEvents_PressedAfterEventHandler(this.Button0_PressedAfter);
+            this.OnCustomInitialize();
+
+        }
+
+        /// <summary>
+        /// Initialize form event. Called by framework before form creation.
+        /// </summary>
+        public override void OnInitializeFormEvents()
+        {
+        }
+
+        private SAPbouiCOM.Button Button0;
+
+        private void OnCustomInitialize()
+        {
+
+        }
+
+        private void Button0_PressedAfter(object sboObject, SAPbouiCOM.SBOItemEventArg pVal)
+        {
+            PostedGRPO ab = new PostedGRPO("1", "2");
+            ab.Show();
+            Form deliveryForm = SAPbouiCOM.Framework.Application.SBO_Application.Forms.ActiveForm;
+            if (deliveryForm.Type == 140)
+            {
+                Documents deliveryObj = (Documents)DiManager.Company.GetBusinessObject(BoObjectTypes.oDeliveryNotes);
+                string deliveryDocEntry = deliveryForm.DataSources.DBDataSources.Item(0).GetValue("DocEntry", 0);
+                if (string.IsNullOrWhiteSpace(deliveryDocEntry))
+                {
+                    Application.SBO_Application.SetStatusBarMessage("დოკუმენტი არაა დამატებული (Delivery)",
+                        BoMessageTime.bmt_Short, true);
+                    return;
+                }
+
+
+                deliveryObj.GetByKey(int.Parse(deliveryDocEntry));
+
+                if (deliveryObj.DocumentStatus == BoStatus.bost_Close)
+                {
+                    Application.SBO_Application.SetStatusBarMessage("უკვე გამოწერილია",
+                        BoMessageTime.bmt_Short, true);
+                    return;
+                }
+
+                var deliveryXmlString = deliveryObj.GetAsXML();
+                XElement DelXml = XElement.Parse(deliveryXmlString);
+                List<string> costs = new List<string>();
+                try
+                {
+                    foreach (var node in DelXml.Element("BO").Element("DLN1").Elements("row").Elements().Where(x => x.Name == "StockPrice"))
+                    {
+                        var cost = node.Value;
+                        costs.Add(cost);
+                    }
+                }
+                catch (Exception e)
+                {
+
+                }
+
+                BusinessPartners businessPartner = (BusinessPartners)DiManager.Company.GetBusinessObject(BoObjectTypes.oBusinessPartners);
+                businessPartner.GetByKey(deliveryObj.CardCode);
+
+
+                string whs = deliveryObj.UserFields.Fields.Item("U_WareHouse").Value.ToString();
+
+                DiManager.Recordset.DoQuery(DiManager.QueryHanaTransalte($"SELECT * FROM OWHS WHERE WhsCode = N'{whs}'"));
+                int branchId = int.Parse(DiManager.Recordset.Fields.Item("BPLid").Value.ToString());
+
+                Documents goodsReceiptPo = (Documents)DiManager.Company.GetBusinessObject(BoObjectTypes.oPurchaseDeliveryNotes);
+                goodsReceiptPo.CardCode = businessPartner.LinkedBusinessPartner;
+                goodsReceiptPo.BPL_IDAssignedToInvoice = branchId;
+                goodsReceiptPo.DocDate = deliveryObj.DocDate;
+
+                for (int i = 0; i < deliveryObj.Lines.Count; i++)
+                {
+                    deliveryObj.Lines.SetCurrentLine(i);
+                    goodsReceiptPo.Lines.ItemCode = deliveryObj.Lines.ItemCode;
+                    goodsReceiptPo.Lines.Quantity = deliveryObj.Lines.Quantity;
+                    goodsReceiptPo.Lines.UnitPrice = double.Parse(costs[i],CultureInfo.InvariantCulture);
+                    goodsReceiptPo.Lines.WarehouseCode = whs;
+                    goodsReceiptPo.Lines.Add();
+                }
+
+                deliveryObj.Close();
+                var res = goodsReceiptPo.Add();
+                if (res == 0)
+                {
+                    string docEntry = DiManager.Company.GetNewObjectKey();
+                    string docNum;
+                    DiManager.Company.GetNewObjectCode(out docNum);
+                    PostedGRPO postedGrpo  = new PostedGRPO(docEntry,docNum);
+                    postedGrpo.Show();
+                }
+                var err = DiManager.Company.GetLastErrorDescription();
+                if (string.IsNullOrWhiteSpace(err))
+                {
+                    SAPbouiCOM.Framework.Application.SBO_Application.StatusBar.SetSystemMessage("წარმატება", BoMessageTime.bmt_Short, BoStatusBarMessageType.smt_Success);
+                }
+                else
+                {
+                    SAPbouiCOM.Framework.Application.SBO_Application.StatusBar.SetSystemMessage(err, BoMessageTime.bmt_Short);
+                }
+            }
+        }
+    }
+}
---
+++ InventoryTransferBranch/PostedGRPO.b1f.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SAPbouiCOM.Framework;
+
+namespace InventoryTransferBranch
+{
+    [FormAttribute("InventoryTransferBranch.PostedGRPO", "PostedGRPO.b1f")]
+    class PostedGRPO : UserFormBase
+    {
+        private readonly string _docEntry;
+        private readonly string _docNum;
+
+        public PostedGRPO(string docEntry, string docNum)
+        {
+            _docEntry = docEntry;
+            _docNum = docNum;
+        }
+
+        /// <summary>
+        /// Initialize components. Called by framework after form created.
+        /// </summary>
+        public override void OnInitializeComponent()
+        {
+            this.EditText0 = ((SAPbouiCOM.EditText)(this.GetItem("Item_0").Specific));
+            this.LinkedButton0 = ((SAPbouiCOM.LinkedButton)(this.GetItem("Item_1").Specific));
+            this.StaticText0 = ((SAPbouiCOM.StaticText)(this.GetItem("Item_2").Specific));
+            this.EditText1 = ((SAPbouiCOM.EditText)(this.GetItem("Item_3").Specific));
+            this.OnCustomInitialize();
+
+        }
+
+        /// <summary>
+        /// Initialize form event. Called by framework before form creation.
+        /// </summary>
+        public override void OnInitializeFormEvents()
+        {
+            this.VisibleAfter += new VisibleAfterHandler(this.Form_VisibleAfter);
+
+        }
+
+        private SAPbouiCOM.EditText EditText0;
+
+        private void OnCustomInitialize()
+        {
+            
+        }
+
+        private SAPbouiCOM.LinkedButton LinkedButton0;
+        private SAPbouiCOM.StaticText StaticText0;
+        private SAPbouiCOM.EditText EditText1;
+
+        private void Form_VisibleAfter(SAPbouiCOM.SBOItemEventArg pVal)
+        {
+           var title =  Application.SBO_Application.Forms.ActiveForm.Title;
+            if (title == "Posted Grpo")
+            {
+                EditText0.Item.Left = 10000;
+                EditText0.Value = _docEntry;
+                EditText1.Value = _docNum;
+            }
+        }
+    }
+}
